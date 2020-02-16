6f26dbd87ffb298ab09380cc00687a4cb0529d5e
AZIREZ23 <239912@gmail>
2020-02-16 16:42:00 +0700

    perbaikan logika

--- exercise8.js
+++ exercise8.js
@@ -3,27 +3,25 @@
 // Diberikan sebuah function pasanganTerbesar(angka) yang menerima 1 parameter berupa angka. Functiona akan menentukan pasangan dua digit angka mana yang paling besar dan me-return angka tersebut. Contoh, jika angka adalah 183928, maka function akan me-return 92, pasangan dua digit angka yang paling besar diantara yang lainnya.
 
 function pasanganTerbesar(num) {
-    // you can only write your code here!
-        
-    var numbers = "";
-    var nilaiStr =  String(num) 
-    var mulai = "0";
-    for ( var i =0; i< nilaiStr.length -1; i++){
-        
-        numbers = nilaiStr[i]+nilaiStr[i+1]
-        
-        if (numbers > 79 || numbers >= 80 ){
-            return numbers
-            // console.log("ini adalah numbers :  "+ numbers)
-            // console.log("ini adalah mulai : "+ mulai)
+    // you can only write your code here!     
+    var numbers = []
+    var tampung = 0
+    var nilaiStr = String(num)
+    for (var i = 0; i < nilaiStr.length - 1; i++) {
+        numbers = nilaiStr[i] + nilaiStr[i + 1]
+        //  tampung.push(numbers)
+        //  console.log(numbers)
+
+        if (numbers > tampung) {
+            tampung = numbers
         }
     }
-    return numbers
-  }
-  
-  // TEST CASES
-  console.log(pasanganTerbesar(641573)); // 73
-  console.log(pasanganTerbesar(12783456)); // 83
-  console.log(pasanganTerbesar(910233)); // 91
-  console.log(pasanganTerbesar(71856421)); // 85
-  console.log(pasanganTerbesar(79918293)); // 99
+    return tampung
+}
+
+// TEST CASES
+console.log(pasanganTerbesar(641573)); // 73
+console.log(pasanganTerbesar(12211111)); // 22
+console.log(pasanganTerbesar(910233)); // 91
+console.log(pasanganTerbesar(71856421)); // 85
+console.log(pasanganTerbesar(79918293)); // 99
